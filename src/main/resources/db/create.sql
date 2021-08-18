d91b69ad37382b1015264a8e74ac07493c0469d3
princepssol <agarwaen260293@gmail.com>
2021-08-19 02:02:46 +0400

    added data to gitignore added properties to database and spring added scripts added docker-compose.yaml with database

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,44 @@
+create schema if not exists oshs;
+
+create sequence if not exists oshs.department_seq start 1 increment 1;
+create sequence if not exists oshs.sex_seq start 1 increment 1;
+create sequence if not exists oshs.position_seq start 1 increment 1;
+create sequence if not exists oshs.employee_seq start 1 increment 1;
+
+create table if not exists oshs.department
+(
+    id        int          not null primary key default nextval('oshs.department_seq'),
+    parent_id int          not null references oshs.department (id),
+    name      varchar(255) not null,
+    created   timestamp    not null
+);
+
+create table if not exists oshs.sex
+(
+    id       int          not null primary key default nextval('oshs.sex_seq'),
+    sex_name varchar(255) not null unique
+);
+
+create table if not exists oshs.position
+(
+    id   int          not null primary key default nextval('oshs.position_seq'),
+    name varchar(255) not null unique
+);
+
+create table if not exists oshs.employee
+(
+    id            int            not null primary key default nextval('oshs.employee_seq'),
+    department_id int            not null references oshs.department (id),
+    sex_id        int            not null references oshs.sex (id),
+    position_id   int            not null references oshs.position (id),
+    leader        boolean        not null,
+    first_name    varchar(255)   not null,
+    second_name   varchar(255),
+    last_name     varchar(255)   not null,
+    birth_date    timestamp      not null,
+    work_date     timestamp      not null,
+    fired_date    timestamp,
+    phone         varchar(255)   not null,
+    email         varchar(255)   not null,
+    money_ru      numeric(15, 2) not null
+);
